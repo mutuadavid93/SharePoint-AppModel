e48f13f12f639fbe959400a18a9f28ff1b8479a2
David Mutua <mutuadavi@gmail.com>
2017-10-04 09:56:30 +0100

    Access App Web Resources

--- SPHostedApp/Scripts/App.js
+++ SPHostedApp/Scripts/App.js
@@ -6,20 +6,51 @@
         ExecuteOrDelayUntilScriptLoaded(initializePage, 'sp.js');
     });
 
+    // Get App Web Resources using CSOM
     function initializePage() {
         var context = SP.ClientContext.get_current();
-        var user = context.get_web().get_currentUser();
 
-        context.load(user);
+        var rootFolder = context.get_web().get_rootFolder();
+        var folders = rootFolder.get_folders();
+
+        var myFolders = context.loadQuery(folders, "Include(Name, Files)");
         context.executeQueryAsync(success, fail);
 
         function success() {
             var message = $('#message');
-            message.text("Hello " + user.get_title());
+            message.text("");
+
+            // iterate through the folders collection
+            // NB: value rep individual Folder
+            $.map(myFolders, function (value, index) {
+                showFiles(message, value);
+            });
         }
 
         function fail(sender, args) {
             alert("Call failed in initializePage(). Error: " + args.get_message());
         }
+
+        // ShowFiles Function
+        function showFiles(message, folder) {
+            var fenum = folder.get_files().getEnumerator();
+            var i = 0;
+            while (fenum.moveNext()) {
+                i++;
+                // Ensure Atleast a Folder has a File
+                if (i == 1) {
+                    message.append(folder.get_name()+ ":");
+                }
+
+                // return the current file Object(i.e. fileName)
+                var name = fenum.get_current().get_name();
+                message.append("<div style='padding-left:10px'>" + name + "</div>");
+            } // while loop
+
+            if (i > 0) {
+                message.append("<br />");
+            }
+        } // ShowFiles()
+
     } // initializePage()
 })();
