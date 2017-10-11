21cf7e350fa7d294e34c42c61f36d456d7407c7b
David Mutua <mutuadavi@gmail.com>
2017-10-11 15:14:48 +0100

    Provisioning - Populating the List

--- SPHostedApp/pkg/Debug/SPHostedApp/SPHostedApp_Feature1/Scripts/provisioner.js
+++ SPHostedApp/pkg/Debug/SPHostedApp/SPHostedApp_Feature1/Scripts/provisioner.js
@@ -55,13 +55,89 @@ Pluralsight.Provisioner = function (appUrl, hostUrl) {
 
         function success() {
             dfd.notify("Products list created");
-            dfd.resolve();
+            getProductsData();
         }
 
         function fail(sender, args) {
             dfd.reject(args);
         }
     }
+
+    // Get the Products List data to Populate it
+    function getProductsData() {
+        if (!dfd) return;
+        dfd.notify("Requesting Products data");
+
+        var url = appUrl + "/Content/ProductsData.txt";
+        var call = $.get(url);
+        call.done(function (data, textStatus, jqXHR) {
+            populateProductsList(data); // invoke populateProductsList
+        });
+        call.fail(function (jqXHR, textStatus, errorThrown) {
+            dfd.reject(jqXHR);
+        });
+    } // getProductsData()
+
+    // Data to Populate the List with data
+    function populateProductsList(data) {
+        if (!dfd) return;
+        dfd.notify("Populating Products list");
+
+        var context = new SP.ClientContext(appUrl);
+        var web = context.get_web();
+        var list = web.get_lists().getByTitle("Products");
+
+        var products = JSON.parse(data);
+        var currentProduct = -1; // counter
+        updateNextSet();
+
+        function updateNextSet(){
+            var setIndex = 0;
+            while (true) {
+                setIndex += 1;
+                currentProduct += 1;
+                if (setIndex == 25 || currentProduct == products.length) {
+                    context.executeQueryAsync(success, fail);
+                    break;
+                }
+
+                // add item to Products list
+                var value = products[currentProduct];
+                var ici = new SP.ListItemCreationInformation();
+                var item = list.addItem(ici);
+
+                // Populate the Field's Values
+                item.set_item("Title", value.Title);
+                item.set_item("QuantityPerUnit", value.QuantityPerUnit);
+                item.set_item("UnitPrice", value.UnitPrice);
+                item.set_item("UnitsInStock", value.UnitsInStock);
+                item.set_item("UnitsOnOrder", value.UnitsOnOrder);
+                item.set_item("ReorderLevel", value.ReorderLevel);
+                item.set_item("Discontinued", value.Discontinued);
+
+                // Handle the Lookup
+                var lfv = new SP.FieldLookupValue();
+                lfv.set_lookupId(value.CategoryId);
+                item.set_item("Category", lfv);
+                item.update();
+
+            }; // while Loop
+        } // updateNextSet()
+
+        function success() {
+            dfd.notify(String.format("\t{0} of {1}", currentProduct, products.length));
+            if (currentProduct == products.length) {
+                //updateCurrentVersion();
+                dfd.resolve();
+            } else {
+                updateNextSet();
+            }
+        }
+
+        function fail(sender, args) {
+            dfd.reject(args);
+        }
+    } //populateProductsList()
 
     // Create a Public Function i.e. Execute
     function execute() {
